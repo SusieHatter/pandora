e06db9290b0f38d689902b041bbd8d59a126d14e
SusieHatter <susiivaldez@gmail.com>
2021-11-07 07:03:54 -0600

    Add /repos and /repos/{repo}/commits

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { getAllCommits } from "./api/github.js";
+import { getAllCommits, getRepos, getRepoCommits } from "./api/github.js";
 
 dotenv.config();
 
@@ -16,6 +16,27 @@ app.use(
 app.get("/github/all_commits", async (req, res) => {
   console.log("fetching all commits");
   const commits = await getAllCommits("SusieHatter", {
+    per_page: 100,
+    author: "SusieHatter",
+  });
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify(commits));
+});
+
+app.get("/github/repos", async (req, res) => {
+  console.log("fetching all repos");
+  const repos = await getRepos("SusieHatter", {
+    per_page: 100,
+    sort: "created_at",
+  });
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify(repos));
+});
+
+app.get("/github/repos/:repoName/commits", async (req, res) => {
+  const repoName = req.params.repoName;
+  console.log(`fetching commits from ${repoName}`);
+  const commits = await getRepoCommits("SusieHatter", repoName, {
     per_page: 100,
     author: "SusieHatter",
   });
